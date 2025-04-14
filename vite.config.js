8494e89fded24f30c4937de69dc9415a3a2662d2
jeevajeyakrishnan <jeeevasuriya@gmail.com>
2025-04-14 21:33:19 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"https://jeevajeyakrishnan.github.io/Stay-at-forest",
+  base:"/Stay-at-forest",
   plugins: [react()],
 })
